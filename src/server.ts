4d730393696f9612054c5234991873cbda8bfebc
Stein Kim <wotjd1102@gmail.com>
2018-08-13 21:48:45 +0900

    Change ormconfig storage format from js to .env

--- src/server.ts
+++ src/server.ts
@@ -4,26 +4,31 @@ import { buildSchema } from "graphql";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 
+async function main() {
+  // create a connection using modified connection options
+  await createConnection();
 
-createConnection().then(async connection => {}).catch(error => console.log(error));
+  const schema = buildSchema(`
+    type Query {
+      hello: String
+    }
+  `);
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+  const root = {
+    hello: () => {
+      return "Hello World";
+    },
+  };
 
-const root = {
-  hello: () => {
-    return "Hello World";
-  },
-};
+  const app = express();
+  app.use("/graphql", graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  }));
+  app.listen(4000);
+  console.log("Running a GraphQL API server at localhost:4000/graphql");
+}
 
-const app = express();
-app.use("/graphql", graphqlHTTP({
-  schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000);
-console.log("Running a GraphQL API server at localhost:4000/graphql");
+main();
+
