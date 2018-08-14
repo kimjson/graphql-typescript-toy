f9d653281a8f438fa02bf0982f50e5d5a312ee3e
Stein Kim <wotjd1102@gmail.com>
2018-08-14 23:38:01 +0900

    Create students table.

--- src/migrations/1534164266169-CreateStudent.ts
+++ src/migrations/1534164266169-CreateStudent.ts
@@ -1,11 +1,31 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateStudent1534164266169 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(new Table({
+      name: "students",
+      columns: [
+        {
+          name: "id",
+          type: "int",
+          isPrimary: true
+        },
+        {
+          name: "name",
+          type: "varchar",
+        },
+        {
+          name: "age",
+          type: "int",
+          isPrimary: true
+        }
+      ]
+    }), true)
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-    }
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable("students");
+  }
 
 }
